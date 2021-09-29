9ac4c9d323debd0f0ad0e33704f4353f40b1ad76
federerj <joel.federer-gsponer@roche.com>
2021-09-29 03:52:06 +0200

    Fixed convers

--- R/line_plot.R
+++ R/line_plot.R
@@ -31,8 +31,8 @@ line_plot <- function(DATA,
                       scales = "free_y",
                       ...) {
   if (!is.null(fill) && is.null(color)) color <- fill
-  if (is.character(summarise_y)) summarise_y <- rlang::sym(summarise_y)
-  if (is.character(theme)) theme <- rlang::sym(theme)
+  if (is.character(summarise_y)) function(x) do.call(summarise_y, list(x))
+  if (is.character(theme)) function(x) do.call(theme, list(x))
   # Add text wraping for facets
   DATA <- DATA %>%
     dplyr::mutate_at(
