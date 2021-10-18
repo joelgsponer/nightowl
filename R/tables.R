b33b91e6dfcfb76dedc3c8a175ef2b67b2bcfb80
federerj <joel.federer-gsponer@roche.com>
2021-10-19 00:45:49 +0200

    Basic functionality down

---
+++ R/tables.R
@@ -0,0 +1,31 @@
+#===============================================================================
+#' Summry table
+#' Table to display data that is being used in plots
+#' @export
+summary_table <- function(DATA,
+                          x,
+                          y,
+                          group = NULL,
+                          facet_cols = NULL,
+                          facet_rows = NULL,
+                          remove_missing = FALSE,
+                          label_width = 20) {
+  #*******************************************************************************
+  # Drop columns that are not needed
+  DATA <- DATA %>%
+    dplyr::select_at(c(x, y, group, facet_cols, facet_rows))
+  #*******************************************************************************
+  # Drop missing values
+  DATA <- nightowl::prepare_data_for_plotting(DATA, remove_missing = remove_missing)
+  # Data preparation
+  DATA <- nightowl::add_text_wraping(DATA, width = label_width)
+  #*******************************************************************************
+  # Build table
+  .lty <- rtables::basic_table()
+  .lty <- purrr::reduce(facet_cols, ~rtables::split_cols_by(.x, .y), .init = .lty)
+  .lty <- purrr::reduce(facet_rows, ~rtables::split_rows_by(.x, .y), .init = .lty)
+  .lty <- rtables::split_cols_by(.lty, x)
+  .lty <- rtables::split_rows_by(.lty, group)
+  .lty <- tern::summarize_vars(.lty, y)
+  rtables::build_table(.lty, DATA)
+}
