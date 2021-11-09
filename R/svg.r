4a20a2b83cf5bd7c6f9dc6a34fafb5b6e63c6efb
federerj <joel.federer-gsponer@roche.com>
2021-11-09 11:17:33 +0100

    Added options for width and height

--- R/svg.r
+++ R/svg.r
@@ -2,12 +2,10 @@
 #' Create SVG string
 #' @param g ggplot object
 #' @export
-render_svg <- function(g) {
-  tryCatch(
-    {
-      svg <- svglite::svgstring()
+render_svg <- function(g, ...) {
+  tryCatch({
+      svg <- svglite::svgstring(...)
       print(g)
-      dev.off()
       svg <- waRRior::regex_replace_element_parameter(svg(), "width", "100%") %>%
         waRRior::regex_replace_element_parameter("height", "100%")
       return(svg)
@@ -15,6 +13,9 @@ render_svg <- function(g) {
     error = function(e) {
       dev.off()
       stop(e)
+    }, finally = function() {
+            dev.off()
     }
   )
 }
+#===============================================================================
