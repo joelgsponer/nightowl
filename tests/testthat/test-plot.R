95dcee502d202514bdafed8f08ac2e91de4ee0ea
federerj <joel.federer-gsponer@roche.com>
2021-11-11 11:16:06 +0100

    Plot is done for now

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,19 +1,25 @@
 test_that("multiplication works", {
+
   testdata <- ChickWeight %>%
     dplyr::filter(Time < 10)
   nightowl::plot(testdata,
     mapping = list(
       x = "Time",
       y = "weight",
-      fill = "Diet"
+      fill = "Diet",
+      lty = "Diet"
     ),
     boxplot = list(),
     violin = list(),
     dotplot = list(),
     points = list(),
-    summary = list(),
+    smooth = list(lm = list()),
+    summary = list(line = list()),
+    annotation = list(title = "TEST"),
+    axis = list(log_y = TRUE, xlim = c(2, 4), units_x = "days", units_y = "g"),
     dodge = 0.5
-  )
+  ) %>%
+  peek()
 
   nightowl::plot(testdata,
     mapping = list(
