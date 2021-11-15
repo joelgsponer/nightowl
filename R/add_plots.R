9555e7bccdbdcd21b3da9c34a671bbdc56751454
federerj <joel.federer-gsponer@roche.com>
2021-11-15 14:10:14 +0100

    made boxplot simpler

--- R/add_plots.R
+++ R/add_plots.R
@@ -19,18 +19,11 @@ add_violin <- function(g,
 add_boxplot <- function(g,
                         mapping,
                         dodge = 0.5,
-                        outlier.shape = NA,
-                        coef = 0,
-                        alpha = 1,
-                        width = 0.2,
-                        color = "black", ...) {
+                        color = "black",
+                        ...) {
   g + ggplot2::geom_boxplot(
     mapping = ggplot2::aes(x = forcats::as_factor(.data[[mapping$x]])),
     position = ggplot2::position_dodge(dodge, preserve = "total"),
-    outlier.shape = outlier.shape,
-    coef = coef,
-    alpha = alpha,
-    width = width,
     color = color,
     ...
   )
