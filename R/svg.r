7878aef0fe63a9a48cafa433d4b81754d69a0a48
Joel Federer-Gsponer <joel.federer-gsponer@roche.com>
2021-12-20 21:10:29 +0100

    another atempt at fixing the font, this time with regex

--- R/svg.r
+++ R/svg.r
@@ -3,7 +3,7 @@
 #' @param g ggplot object
 #' @export
 render_svg <- function(g, height = 8, width = 8, scaling = 1) {
-  fix_font <- function(html, param = "font-family", old_value = "Arial", value = "Lato") {
+  fix_font <- function(html, param = "font-family", value = "Lato", old_value = "[^;]*") {
     str <- as.character(html)
     pattern <- glue::glue("{param}: {old_value}")
     print(pattern)
@@ -13,12 +13,14 @@ render_svg <- function(g, height = 8, width = 8, scaling = 1) {
   }
   tryCatch(
     {
-      svg <- svglite::svgstring(height = height, width = width, scaling = scaling)
+      svg <- svglite::svgstring(
+        height = height, width = width, scaling = scaling,
+        web_fonts = "https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
+      )
       print(g)
       svg <- waRRior::regex_replace_element_parameter(svg(), "width", "100%") %>%
         waRRior::regex_replace_element_parameter("height", "100%") %>%
-        fix_font() %>%
-        fix_font(old_value = "Liberation Sans")
+        fix_font()
       try(dev.off())
       return(svg)
     },
