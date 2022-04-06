164914a3d9aeb97a5694a27cf60f4d0d18f5af66
Joel Federer-Gsponer <joel.federer-gsponer@roche.com>
2022-04-06 10:32:02 +0200

    Added  download button

--- R/svg.r
+++ R/svg.r
@@ -2,7 +2,7 @@
 #' Create SVG string
 #' @param g ggplot object
 #' @export
-render_svg <- function(g, height = 8, width = 8, scaling = 1) {
+render_svg <- function(g, height = 8, width = 8, scaling = 1, add_download_button = T) {
   fix_font <- function(html, param = "font-family", value = "Lato", old_value = "[^;]*") {
     str <- as.character(html)
     pattern <- glue::glue("{param}: {old_value}")
@@ -23,6 +23,9 @@ render_svg <- function(g, height = 8, width = 8, scaling = 1) {
         fix_font() %>%
         htmltools::browsable()
       try(dev.off())
+      if (add_download_button) {
+        svg <- nightowl::add_download_button(svg)
+      }
       return(svg)
     },
     error = function(e) {
@@ -38,6 +41,44 @@ peek <- function(g, ...) {
   nightowl::render_svg(g, ...) %>%
     htmltools::browsable()
 }
+# =================================================
+# =================================================
+#' @title
+#' MISSING_TITLE
+#' @description
+#' @detail
+#' @param
+#' @return
+#' @export
+add_download_button <- function(x) {
+  shiny::div(
+    lowRider::useLowRider(force = F),
+    shiny::HTML('
+      <script>
+          function download(e) {
+            var svg = e.parentElement.querySelector(\"svg\");
 
-# ===============================================================================
+            var svgXml = (new XMLSerializer).serializeToString(svg);
+            var blob = new Blob([svgXml], {type: \"image/svg+xml\"});
+            var url = URL.createObjectURL(blob);
+
+            var link = document.createElement(\'a\');
+            link.href = url;
+            link.download = \"image.svg\";
+            link.style.display = \'none\';
+            document.body.appendChild(link);
+
+            // your code goes here
+            link.click();
+            document.body.removeChild(link);
+          };
+      </script>
+    '),
+    x,
+    shiny::HTML("
+      <button onclick='download(this)', class = 'lowrider-button lowrider-button-small'><i class=\"fa fa-download\" aria-hidden=\"true\"></i> Save</button>
+    ")
+  ) %>%
+    htmltools::browsable()
+}
 # =================================================
--- tests/testthat/test-svg.R
+++ tests/testthat/test-svg.R
@@ -2,7 +2,8 @@ test_that("svg works", {
   library(magrittr)
   testdata <- ChickWeight %>%
     dplyr::filter(Time < 10)
-  nightowl::plot(testdata,
+
+  .p <- nightowl::plot(testdata,
     transform = list(x = "factor"),
     mapping = list(
       x = "Time",
@@ -16,9 +17,9 @@ test_that("svg works", {
       list(type = "traces", geom = "line", alpha = 0.3),
       list(type = "summary", mapping = list(color = NULL), geom = "line", dodge = 0, size = 1.5)
     )
-  ) %>%
-    nightowl::render_svg(height = 50, width = 40, scaling = 3)
+  )
 
-  replace_all("font-family", "Lato") ->
-  a
+  nightowl::render_svg(.p, height = 4, width = 4, scaling = 1)
+  nightowl::render_svg(.p, height = 4, width = 8, scaling = 0.5)
+  nightowl::render_svg(.p, height = 3, width = 8, scaling = 0.5, add_download_button = F)
 })
