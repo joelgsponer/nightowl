04b4fe8fe2133fe95959759debe6210e0af5da97
Joel Federer-Gsponer <joel.federer-gsponer@roche.com>
2022-05-09 10:21:54 +0200

    Startet to work on ANOVA

--- tests/testthat/test-ancova.R
+++ tests/testthat/test-ancova.R
@@ -1,14 +1,12 @@
 test_that("ANCOVA", {
-
-      testdata = data.frame(
-            y = runif(100),
-            x = sample(c("A", "B", "C"), 100, TRUE)
-      )
-      m <- lm(y ~ x, data = testdata)
-      aov(y ~ x, data = testdata)
-      a <- anova(m)
-      car::leveneTest(m)
-      car::Anova(m) 
-      car::Anova(m, type="III") 
-
+  testdata <- data.frame(
+    y = runif(100),
+    x = sample(c("A", "B", "C"), 100, TRUE)
+  )
+  m <- lm(y ~ x, data = testdata)
+  aov(y ~ x, data = testdata)
+  a <- anova(m)
+  car::leveneTest(m)
+  car::Anova(m)
+  car::Anova(m, type = "III")
 })
