793b393502d6e45542a88a33f8800007fb1ef1fa
joelgsponer <joel.federer-gsponer@roche.com>
2022-07-13 22:29:14 +0200

    added labels

--- R/coxph.R
+++ R/coxph.R
@@ -5,7 +5,7 @@
 #' @export
 Coxph <- R6::R6Class("Coxph",
   public = list(
-    initialize = function(...) {
+    initialize = function(..., labels = NULL) {
       purrr::imap(list(...), function(.x, .y) {
         if (.y %in% names(self)) {
           self[[.y]] <- .x
@@ -13,6 +13,7 @@ Coxph <- R6::R6Class("Coxph",
       })
       # Set data
       self$set_data()
+      self$set_labels(labels)
       self$set_reference()
       self$set_formula()
       self$fit()
@@ -136,7 +137,6 @@ Coxph <- R6::R6Class("Coxph",
     },
     # Model --------------------------------------------------------------------
     models = NULL,
-    results = NULL,
     args_model = NULL,
     fit = function() {
       self$check_data()
@@ -148,7 +148,7 @@ Coxph <- R6::R6Class("Coxph",
       if (!dplyr::is_grouped_df(self$data)) {
         data_list <- list(ALL = self$data)
       } else {
-        data_list <- waRRior::named_group_split_at(self$data, self$group_by, keep = T)
+        data_list <- waRRior::named_group_split_at(self$data, self$group_by, keep = T, verbose = T)
       }
       self$models <- purrr::map(data_list, purrr::safely(function(.data) {
         do.call(
@@ -162,21 +162,261 @@ Coxph <- R6::R6Class("Coxph",
           )
         )
       }))
-      self$results <- purrr::map(self$models, "result") %>%
-        purrr::map(~ broom::tidy(.x))
-      invisible(self)
+    },
+    # Results ------------------------------------------------------------------
+    exponentiate = TRUE,
+    log_x = TRUE,
+    results = function() {
+      variables <- self$get_variables()
+      purrr::map(self$models, "result") %>%
+      purrr::imap(function(.result, .group){
+      .result %>%
+        broom::tidy(exponentiate = self$exponentiate, conf.int = TRUE) %>%
+        dplyr::mutate(Subgroup = .group) %>%
+        purrr::reduce(c(variables$treatment, variables$covariates), function(.in, .cov) {
+          .in$term <- stringr::str_replace(.in$term, .cov, paste0(.cov, "splithere"))
+          .in
+        }, .init = .) %>%
+        {
+          x <- .
+          x$comparison <- stringr::str_split(x$term, stringr::fixed("splithere")) %>%
+            purrr::map(~ .x[[2]]) %>%
+            unlist()
+          x$term <- stringr::str_split(x$term, stringr::fixed("splithere")) %>%
+            purrr::map(~ .x[[1]]) %>%
+            unlist()
+          x
+        } %>%
+        dplyr::mutate_if(is.numeric, ~ round(.x, 4)) %>%
+        dplyr::arrange(estimate) %>%
+        dplyr::mutate(term = factor(term, c(variables$treatment, unique(waRRior::pop(.$term, variables$treatment))))) %>%
+        dplyr::arrange(term) %>%
+        dplyr::mutate(reference = purrr::map_chr(as.character(term), ~ self$get_reference()[[.x]])) %>%
+        dplyr::select(Subgroup, term, reference, comparison, estimate, tidyselect::everything())
+     })
+
+    },
+    # Errors -------------------------------------------------------------------
+    errors = function(){
+      purrr::map(self$models, "error")
+    },
+    # N ------------------------------------------------------------------
+    N = function(){
+      variables <- self$get_variables()
+      data_list <- waRRior::named_group_split_at(self$data, self$group_by, keep = T, verbose = T)
+      purrr::imap(data_list, function(.data, .subgroup){
+        res <- purrr::map(c(variables$treatment, self$covariates, self$strata), function(.var) {
+          if (!is.numeric(.data[[.var]])) {
+            dplyr::select_at(.data, c(.var, variables$event)) %>%
+              waRRior::tally_at(c(.var, variables$event)) %>%
+              dplyr::mutate(!!rlang::sym(variables$event) := as.character(!!rlang::sym(variables$event))) %>%
+              dplyr::select_at(c(.var, "n", variables$event)) %>%
+              dplyr::rename(comparison = .var) %>%
+              dplyr::mutate(term = .var) %>%
+              # dplyr::rename(`N Events` = n) %>%
+              dplyr::mutate(comparison = as.character(comparison))
+          } else {
+            dplyr::select_at(data, c(.var, event)) %>%
+              waRRior::tally_at(c(event)) %>%
+              dplyr::mutate(!!rlang::sym(variables$event) := as.character(!!rlang::sym(variables$event))) %>%
+              dplyr::select_at(c(.var, "n", variables$event)) %>%
+              dplyr::select_at(c("n")) %>%
+              dplyr::mutate(term = .var) %>%
+              # dplyr::rename(`N Events` = n) %>%
+              dplyr::mutate(comparison = "")
+          }
+        }) %>%
+          dplyr::bind_rows()
+        res %>%
+         dplyr::group_by_at(c("comparison", "term")) %>%
+         dplyr::summarise(N = sum(n, na.rm = T), event = !!rlang::sym(variables$event)) %>%
+         dplyr::inner_join(res) %>%
+         dplyr::mutate(`N Events` = paste(n, N, sep = "/")) %>%
+         dplyr::filter(!!rlang::sym(variables$event) == "1") %>%
+         waRRior::drop_columns(c("n", "N", variables$event)) %>%
+         dplyr::mutate(Subgroup = .subgroup)
+      }) %>%
+      dplyr::bind_rows()
+    },
+    # Annotation ----------------------------------------------------------------
+    add_caption = TRUE,
+    caption = function() {
+      variables <- self$get_variables()
+      if (self$add_caption) {
+        if (is.null(variables$covariates) && is.null(variables$strata)) {
+          .title <- "Univariate Cox's Proportional Hazard Model"
+        } else {
+          .title <- "Multivariate Cox's Proportional Hazard Model"
+        }
+        .title
+      } else {
+        NULL
+      }
+    },
+    add_footnote = TRUE,
+    footnote = function() {
+      variables <- self$get_variables()
+      if (self$add_footnote) {
+        if (is.null(variables$covariates) && is.null(variables$strata)) {
+          .footnote <- "Univariate Analysis"
+        } else {
+          comb <- c(variables$covariates, variables$strata)
+          if (is.null(self$labels)){
+            labels <- comb %>% purrr::set_names(comb)
+          } else{
+            labels <- self$labels
+          } 
+          .labeled_covariates <- labels[variables$covariates]
+          .labeled_covariates[is.na(.labeled_covariates)] <- variables$covariates[is.na(.labeled_covariates)]
+          .labeled_strata <- labels[variables$strata]
+          .labeled_strata[is.na(.labeled_strata)] <- variables$strata[is.na(.labeled_strata)]
+
+          if (length(.labeled_covariates) == 0) .labeled_covariates <- "none"
+          if (length(.labeled_strata) == 0) .labeled_strata <- "none"
+          .footnote <- paste(
+            stringr::str_wrap(paste("Covariates: ", paste(.labeled_covariates, collapse = "; ")), width = 200),
+            stringr::str_wrap(paste("Stratified by: ", paste(.labeled_strata, collapse = "; ")), width = 200),
+            sep = "\n"
+          )
+        }
+        .footnote
+      } else {
+        NULL
+      }
+    },
+    # Labels ------------------------------------------------------------------
+    labels = NULL,
+    set_labels = function(labels) {
+      if (!is.null(labels)) {
+        .data <- self$data
+        names(.data) <- nightowl::get_labels(names(.data), labels)
+        self$data <- .data
+        self$get_variables() %>%
+          purrr::iwalk(function(.x, .y){
+           self[[.y]] <- nightowl::get_labels(.x, labels)
+          })
+      }
+      self$labels <- labels
+    },
+    # Arrange ------------------------------------------------------------------
+    arrange_by = NULL,
+    arrange = function(x) {
+      if (!is.null(self$arrange_by)) {
+        dplyr::arrange(x, !!rlang::sym(self$arrange_by))
+      } else {
+        x
+      }
+    },
+    # Raw ---------------------------------------------------------------------
+    conf_range = NULL,
+    label_left = "Comparison better",
+    label_right = "Reference better",
+    raw = function(drop = NULL, keep_only_treatment = TRUE){
+
+      results <- dplyr::inner_join(
+        self$N(),
+        dplyr::bind_rows(self$results())
+      )
+
+      if(is.null(self$conf_range)){
+        self$conf_range <- c(
+          min(results$conf.low[results$conf.low != -Inf], na.rm = T),
+          max(results$conf.high[results$conf.high != Inf], na.rm = T)
+        )
+      }
+      res <- results %>%
+        dplyr::group_by_all() %>%
+        dplyr::group_split() %>%
+        purrr::map(function(.x) {
+          .p <- nightowl::add_inline_forestplot(
+            x = log(.x$estimate),
+            xmin = log(.x$conf.low),
+            xmax = log(.x$conf.high),
+            xlim = log(self$conf_range),
+            xintercept = 0
+          )
+          .x$Visualization <- .p
+          .x
+        }) %>%
+        dplyr::bind_rows()
+      if(keep_only_treatment){
+        res <- res %>%
+          dplyr::filter(term == self$get_variables()$treatment)
+      }
+      forest_label <- glue::glue("
+        <div 
+        style= 'font-size: smaller;display:flex; flex-direction:column;align-items:center;'>
+        <div>log(HR)</div>
+        <div>← {self$label_left} | {self$label_right} →</div>
+        </div>
+      ")
+      res <- res %>%
+        dplyr::arrange(`estimate`) %>%
+        dplyr::mutate(`conf.low` = round(`conf.low`, 2)) %>%
+        dplyr::mutate(`conf.high` = round(`conf.high`, 2)) %>%
+        dplyr::mutate_if(is.numeric, ~ round(.x, 3)) %>%
+        dplyr::mutate(HR = glue::glue("{estimate} ({conf.low}-{conf.high})")) %>%
+        dplyr::select(
+          Subgroup,
+          Term = term,
+          Comparison = comparison,
+          Reference = reference,
+          !!rlang::sym(forest_label) := Visualization,
+          HR,
+          `p Value` = p.value,
+          `N Events` = `N Events`,
+        )
+        res$`p Value` <- purrr::map_chr(res$`p Value`, ~ nightowl::format_p_value(.x))
+        if (!is.null(drop)) {
+          res <- waRRior::drop_columns(res, drop)
+        }
+        subgroups <- stringr::str_split(res$Subgroup, " / ") %>%
+        purrr::map(~stringr::str_split(.x, "==")) %>%
+        purrr::map(function(.x){
+          purrr::map(.x, function(.y){
+            .res <- list()
+            .res[[.y[[1]]]] <- .y[[2]]
+            tibble::as_tibble(.res)
+          }) %>%
+          dplyr::bind_cols()
+       }) %>%
+       dplyr::bind_rows()
+       
+      dplyr::bind_cols(subgroups, res) %>%
+        dplyr::select(-Subgroup)
+    },
+    # Kabel ------------------------------------------------------------------
+    options_kable = list(),
+    kable = function(drop = NULL, keep_only_treatment = TRUE) {
+      do.call(nightowl::render_kable, c(list(.tbl = self$raw(drop = drop, keep_only_treatment = keep_only_treatment), caption = self$caption(), footnote = self$footnote()), self$options_kable))
+    },
+    # HTML ---------------------------------------------------------------------
+    html = function(drop = NULL) {
+      shiny::HTML(self$kable(drop = drop))
+    },
+    # Reactables ---------------------------------------------------------------
+    options_reactables = list(defaultPageSize = 30),
+    reactable = function(drop = NULL, keep_only_treatment = FALSE, ...) {
+      .args <- list(...)
+      .options <- c(.args, self$options_reactables[!names(self$options_reactables) %in% names(.args)])
+      res <- shiny::div(
+        style = "
+          font-family: 'Lato', sans-serif;
+          display: flex;
+          flex-direction: column;
+          align-content: flex-start;
+          align-items: center;
+        ",
+        shiny::h3(self$caption()),
+        do.call(nightowl::render_reactable, c(list(.tbl = self$raw(drop = drop, keep_only_treatment = keep_only_treatment)), .options)),
+        shiny::div(
+          style = "font-size: 0.8em;",
+          glue::glue(self$footnote())
+        )
+      ) %>%
+        htmltools::browsable()
+      return(res)
     }
-    # labels = NULL
-    # set_labels = function(labels) {
-    #   if (!is.null(labels)) {
-    #     .data <- self$data
-    #     names(.data) <- nightowl::get_labels(names(.data), labels)
-    #     self$data <- .data
-    #     self$column <- nightowl::get_labels(self$column, labels)
-    #     self$group_by <- nightowl::get_labels(self$group_by, labels)
-    #   }
-    #   self$labels <- labels
-    # }
   )
 )
 # =================================================
@@ -315,7 +555,7 @@ plot_coxph <- function(data,
     dplyr::group_by_all() %>%
     dplyr::group_split() %>%
     purrr::map(function(.x) {
-      .p <- nightowl::forestplot(
+      .p <- nightowl::add_inline_forestplot(
         x = .x$estimate,
         xmin = .x$conf.low,
         xmax = .x$conf.high,
@@ -366,9 +606,9 @@ plot_coxph <- function(data,
   }
 
   # Add Scale
-  if (add_scale) {
-    .result <- nightowl::add_scale(.result, scaling = 2.9, height = 0.6)
-  }
+  # if (add_scale) {
+  #   .result <- nightowl::add_scale(.result, scaling = 2.9, height = 0.6)
+  # }
 
   if (is.null(covariates) && is.null(strata)) {
     .footnote <- "Univariate Analysis"
@@ -388,35 +628,35 @@ plot_coxph <- function(data,
       sep = "\n"
     )
   }
-
-  if (engine == "kable") {
-    options(knitr.kable.NA = "")
-    .result %>%
-      knitr::kable("html", escape = F, caption = glue::glue("Cox's Proportional Hazard Model {title}")) %>%
-      kableExtra::column_spec(1, bold = T) %>%
-      kableExtra::collapse_rows(columns = collapse_this, valign = "top") %>%
-      kableExtra::footnote(.footnote) %>%
-      kableExtra::kable_styling(full_width = F)
-  } else if (engine == "reactable") {
-    col_def <- list()
-    col_def[[forest_label]] <- reactable::colDef(html = TRUE, minWidt = 200)
-    tbl <- .result %>%
-      reactable::reactable(
-        columns = col_def,
-        filterable = T,
-        style = list(fontFamily = "Work Sans, sans-serif", fontSize = "14px", padding = "10px"),
-        searchable = TRUE,
-        bordered = TRUE,
-        onClick = "select",
-        showPageSizeOptions = TRUE,
-        pageSizeOptions = c(10, 25, 50, 100)
-      )
-    shiny::div(
-      shiny::h3(glue::glue("Cox's Proportional Hazard Model {title}")),
-      tbl,
-      stringr::str_replace(.footnote, "\n", "<br>")
-    ) %>%
-      htmltools::browsable()
-  }
+  .result
+  # if (engine == "kable") {
+  #   options(knitr.kable.NA = "")
+  #   .result %>%
+  #     knitr::kable("html", escape = F, caption = glue::glue("Cox's Proportional Hazard Model {title}")) %>%
+  #     kableExtra::column_spec(1, bold = T) %>%
+  #     kableExtra::collapse_rows(columns = collapse_this, valign = "top") %>%
+  #     kableExtra::footnote(.footnote) %>%
+  #     kableExtra::kable_styling(full_width = F)
+  # } else if (engine == "reactable") {
+  #   col_def <- list()
+  #   col_def[[forest_label]] <- reactable::colDef(html = TRUE, minWidt = 200)
+  #   tbl <- .result %>%
+  #     reactable::reactable(
+  #       columns = col_def,
+  #       filterable = T,
+  #       style = list(fontFamily = "Work Sans, sans-serif", fontSize = "14px", padding = "10px"),
+  #       searchable = TRUE,
+  #       bordered = TRUE,
+  #       onClick = "select",
+  #       showPageSizeOptions = TRUE,
+  #       pageSizeOptions = c(10, 25, 50, 100)
+  #     )
+  #   shiny::div(
+  #     shiny::h3(glue::glue("Cox's Proportional Hazard Model {title}")),
+  #     tbl,
+  #     stringr::str_replace(.footnote, "\n", "<br>")
+  #   ) %>%
+  #     htmltools::browsable()
+  # }
 }
 # =================================================
