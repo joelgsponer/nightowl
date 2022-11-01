792aaf7c9862e0c63ace079a084fe7dd798d7182
joelgsponer <joel.federer-gsponer@roche.com>
2022-11-01 16:30:05 +0100

    deleted old plot function because of namespacing issues with base

--- R/legacy-plot.R
+++
@@ -1,8 +0,0 @@
-# ===============================================================================
-#' Plot
-#' @importFrom ggplot2 aes mean_cl_boot mean_cl_normal mean_se mean_sdl label_both
-#' @export
-plot <- function(...) {
-  nightowl::DeclarativePlot$new(...)
-}
-# ===============================================================================
